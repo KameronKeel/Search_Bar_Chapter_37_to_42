edc020e1d4d4ac4eb22c85ef8792fe0d5cc48291
Kameron Kkeel <Kkeel3@live.spcollege.edu>
2020-10-03 12:04:58 -0400

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Kameron Keel on 10/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
