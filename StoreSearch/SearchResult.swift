e30860667b8f5aa77f39c8e28aa9214c6dfef118
Kameron Kkeel <Kkeel3@live.spcollege.edu>
2020-10-03 15:05:41 -0400

    Completed chapter 37

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Kameron Keel on 10/3/20.
+//
+
+import Foundation
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
