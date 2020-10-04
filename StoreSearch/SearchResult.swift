f9447c9ecf464959c0dec5f750654e5737428a8c
Kameron Kkeel <Kkeel3@live.spcollege.edu>
2020-10-04 10:22:22 -0400

    Completed chapter 39

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -5,8 +5,96 @@
 //  Created by Kameron Keel on 10/3/20.
 //
 
-import Foundation
-class SearchResult {
-    var name = ""
-    var artistName = ""
+class ResultArray: Codable {
+    var resultCount = 0
+    var results = [SearchResult]()
+}
+
+class SearchResult:Codable, CustomStringConvertible {
+    var description: String{
+        return "Kind: \(kind ?? "None"), Name \(name), Artist Name: \(artistName ?? "None")"
+    }
+    var kind: String? = ""
+    var artistName: String? = ""
+    var trackName: String? = ""
+    var trackPrice: Double? = 0.0
+    var currency = ""
+    var imageSmall = ""
+    var imageLarge = ""
+    var name:String {
+        return trackName ?? collectionName ?? ""
+    }
+    var trackViewUrl: String?
+    var collectionName: String?
+    var collectionViewUrl: String?
+    var collectionPrice: Double?
+    var itemPrice: Double?
+    var itemGenre: String?
+    var bookGenre: [String]?
+    
+    var storeURL: String {
+        return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+    
+    var price: Double {
+        return trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+    
+    var genre: String {
+        if let genre = itemGenre{
+            return genre
+        }else if let genres = bookGenre {
+            return genres.joined(separator: ", ")
+        }
+        return ""
+    }
+    
+    var type: String{
+        let kind = self.kind ?? "audiobook"
+        switch kind {
+        case "album":
+        return "Album"
+        case "audiobook":
+        return "Audiobook"
+        case "book":
+        return "Book"
+        case "ebook":
+        return "E-book"
+        case "feature-movie":
+        return "Movie"
+        case "music-video":
+        return "Music Video"
+        case "podcast":
+        return "Podcast"
+        case "software":
+        return "App"
+        case "song":
+        return "Song"
+        case "tv-episode":
+        return "TV Episode"
+        default:
+        break
+        }
+        return "Unknown"
+    }
+    
+    var artist: String {
+        return artistName ?? ""
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case imageSmall = "imageUrl60"
+        case imageLarge = "imageUrl100"
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genres"
+        case itemPrice = "price"
+        case kind, artistName, currency
+        case trackName, trackPrice, trackViewUrl
+        case collectionName, collectionViewUrl, collectionPrice
+    }
+    
+}
+func < (lhs: SearchResult, rhs: SearchResult) -> Bool {
+    return lhs.name.localizedStandardCompare(rhs.name) == .orderedAscending
 }
